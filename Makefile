91b094102f5b65672e5481f2af5714940a7cc5f9
CodeMonk123 <mg20330012@smail.nju.edu.cn>
2020-10-27 17:44:03 +0800

    argparse set true

--- Makefile
+++ Makefile
@@ -1,19 +1,22 @@
 CC = g++
-CFLAGS = -g -std=c++11 -lclang
+CPPFLAGS = -g -std=c++11 -lclang
 LLVM_INCLUDE = /usr/lib/llvm-10/include
 LLVM_LIB = /usr/lib/llvm-10/lib
 
 codesim: clean ast-parser
-	$(CC) -o codesim AST.o ASTParser.o src/main.cpp -I ./include -I $(LLVM_INCLUDE) -L $(LLVM_LIB) $(CFLAGS)
+	$(CC) -o codesim AST.o ASTParser.o src/main.cpp -I ./include -I ./lib/include  -I $(LLVM_INCLUDE) -L $(LLVM_LIB) $(CPPFLAGS)
 
 ast-parser: 
-	$(CC) -c src/AST.cpp src/ASTParser.cpp  -I ./include -I $(LLVM_INCLUDE) -L $(LLVM_LIB) $(CFLAGS)
+	$(CC) -c src/AST.cpp src/ASTParser.cpp  -I ./include -I ./lib/include -I $(LLVM_INCLUDE) -L $(LLVM_LIB) $(CPPFLAGS)
 
 
 
-.PHONY: clean
+.PHONY: clean test
 
 clean:
 	rm -f *.o
 	rm -f *~
 	rm -f codesim
+
+test: codesim
+	./codesim test1 test2 --verbose
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,78 @@
-#include<ASTParser.hpp>
-
+#include <ASTParser.hpp>
+#include <ezOptionParser.hpp>
 using namespace std;
 
-int main() {
-    string filename;
-    // cin >> filename;
-    filename = "./test/testcase1.hpp";
-    ASTParser parser = ASTParser(filename);
-    parser.parseTheAST();
+void Usage(ez::ezOptionParser &opt)
+{
+    std::string usage;
+    opt.getUsage(usage);
+    std::cout << usage;
+};
+
+// int main()
+// {
+//     string filename;
+//     cin >> filename;
+//     ASTParser parser = ASTParser(filename);
+//     parser.parseTheAST();
+// }
+
+int main(int argc, const char *argv[])
+{
+    ez::ezOptionParser opt;
+    opt.overview = "Codesim aims to find the similarity of two cpp/hpp files.";
+    opt.syntax = "./codesim FIRST SECOND [OPTIONS]";
+    opt.example = "./codesim A.cpp B.cpp --verbose\n";
+    opt.footer = "Codesim 0.0.1, developed by CuiZihan\n";
+
+    opt.add(
+        "",
+        0,
+        0,
+        0,
+        "verbose (display runtime information)",
+        "-v",
+        "--verbose");
+
+    opt.add(
+        "",                            // Default.
+        0,                             // Required?
+        0,                             // Number of args expected.
+        0,                             // Delimiter if expecting multiple args.
+        "Display usage instructions.", // Help description.
+        "-h",                          // Flag token.
+        "-help",                       // Flag token.
+        "--help",                      // Flag token.
+        "--usage"                      // Flag token.
+    );
+
+    opt.parse(argc, argv);
+
+    if (opt.isSet("-h"))
+    {
+        Usage(opt);
+        return 0;
+    }
+
+    bool verboseSet = false;
+    if (opt.isSet("-v"))
+    {
+        verboseSet = true;
+        cout << "verbose set true" << endl;
+    }
+
+    std::string firstFile, secondFile;
+    if (opt.firstArgs.size() != 3)
+    {
+    std:
+        cerr << "ERROR: Expected 2 arguments. Given" << opt.firstArgs.size() - 1 << endl;
+        Usage(opt);
+        return 0;
+    }
+
+    firstFile = *opt.firstArgs[1];
+    secondFile = *opt.firstArgs[2];
+
+    std::cout << "Analyze: " << firstFile << " " << secondFile << endl;
+    return 0;
 }
